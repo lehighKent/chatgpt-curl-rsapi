af5f1543541d9dd128963d77688142b4d7536e4a
lehighKent <kent.orr@lehigh.com>
2023-02-17 12:58:41 -0500

    convert from httr to curl

--- R/gpt_get_completions.R
+++ R/gpt_get_completions.R
@@ -6,10 +6,11 @@
 #' @importFrom httr add_headers content content_type_json POST
 #' @importFrom jsonlite toJSON
 #'
-gpt_get_completions <- function(prompt, openai_api_key = Sys.getenv("OPENAI_API_KEY")) {
+gpt_get_completions <- function(prompt, openai_api_key = config::get()$chatgpt) {
   if (nchar(openai_api_key) == 0) {
     stop("`OPENAI_API_KEY` not provided.")
   }
+
   # See https://beta.openai.com/docs/api-reference/completions/create
   params <- list(
     model = Sys.getenv("OPENAI_MODEL", "text-davinci-003"),
@@ -19,13 +20,20 @@ gpt_get_completions <- function(prompt, openai_api_key = Sys.getenv("OPENAI_API_
     frequency_penalty = as.numeric(Sys.getenv("OPENAI_FREQUENCY_PENALTY", 0)),
     presence_penalty = as.numeric(Sys.getenv("OPENAI_PRESENCE_PENALTY", 0))
   )
+
   if (as.logical(Sys.getenv("OPENAI_VERBOSE", TRUE))) {
     cat(paste0("\n*** ChatGPT input:\n\n", prompt, "\n"))
   }
-  content(POST(
-    "https://api.openai.com/v1/completions",
-    add_headers("Authorization" = paste("Bearer", openai_api_key)),
-    content_type_json(),
-    body = toJSON(c(params, list(prompt = prompt)), auto_unbox = TRUE)
-  ))
+
+  h <- curl::new_handle() |>
+    curl::handle_setheaders("Authorization"=paste("Bearer", openai_api_key)
+                            , "Content-Type"="application/json") |>
+    curl::handle_setopt(customrequest = "POST") |>
+    curl::handle_setopt(postfields = jsonlite::toJSON(c(params, prompt = prompt), auto_unbox = T))
+
+  response <- curl::curl_fetch_memory(url = "https://api.openai.com/v1/completions"
+                                      , handle = h)
+
+  response$content |> rawToChar() |> jsonlite::fromJSON()
+
 }
